36f0ee1814cdbf2e03862b116a880b9181d5d8ac
Florian Brunner <florian-home@web.de>
2023-04-29 22:01:28 +0200

    Add clicki Pictures

--- WebshopFrontend/lib/home/view/pages/shop/first_shopscreen.dart
+++ WebshopFrontend/lib/home/view/pages/shop/first_shopscreen.dart
@@ -1,117 +1,183 @@
+
+
 import 'package:flutter/material.dart';
 
 class FirstShopScreen extends StatelessWidget {
 
+
+
   //TODO: Refactor Button in one Widget
   @override
   Widget build(BuildContext context) {
+
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Stack(
       children: [
-        Positioned(
-          top: 30,
-          left: 0,
-          right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Color(0xFFFFA54F),
-                    backgroundColor: Color(0xff4A6840),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                      minimumSize: Size(150, 50),
-                  ),
-                    onPressed: (){},
-                    child: Text('Taschen & Rucksäcke')
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Color(0xFFFFA54F),
-                      backgroundColor: Color(0xff4A6840),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(150, 50),
-                    ),
-                    onPressed: (){},
-                    child: Text('Kopfbedeckung')
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Color(0xFFFFA54F),
-                      backgroundColor: Color(0xff4A6840),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(150, 50),
-                    ),
-                    onPressed: (){},
-                    child: Text('Kinder & Baby\'s')
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Color(0xFFFFA54F),
-                      backgroundColor: Color(0xff4A6840),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(150, 50),
-                    ),
-                    onPressed: (){},
-                    child: Text('Kissen & Decken')
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Color(0xFFFFA54F),
-                      backgroundColor: Color(0xff4A6840),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(150, 50),
-                    ),
-                    onPressed: (){},
-                    child: Text('Kuscheltiere')
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Color(0xFFFFA54F),
-                      backgroundColor: Color(0xff4A6840),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(150, 50),
-                    ),
-                    onPressed: (){},
-                    child: Text('Socken')
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Color(0xFFFFA54F),
-                      backgroundColor: Color(0xff4A6840),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: Size(150, 50),
-                    ),
-                    onPressed: (){},
-                    child: Text('Schals & Tücher')
-                ),
-              ],
-            ),
-          ),
-
-        Center(
-          child: Text('Hello, World!'),
+        FourPictureRow(
+          topPosition: screenHeight * 0.08,
+          pictureSizeHeight: 300,
+          pictureSizeWidth: 300,
+          picture1: '../assets/StartScreen.png',
+          picture2: '../assets/StartScreen.png',
+          picture3: '../assets/StartScreen.png',
+          picture4: '../assets/StartScreen.png',
+        ),
+        FourPictureRow(
+          topPosition: screenHeight * 0.5,
+          pictureSizeHeight: 300,
+          pictureSizeWidth: 300,
+          picture1: '../assets/StartScreen.png',
+          picture2: '../assets/StartScreen.png',
+          picture3: '../assets/StartScreen.png',
+          picture4: '../assets/StartScreen.png',
         ),
       ],
     );
   }
 }
+
+class FourPictureRow extends StatelessWidget {
+  const FourPictureRow({
+    super.key, required this.topPosition, required this.pictureSizeWidth, required this.pictureSizeHeight, required this.picture1, required this.picture2,  required this.picture3, required this.picture4,
+  });
+
+  final topPosition;
+  final pictureSizeWidth;
+  final pictureSizeHeight;
+  final picture1;
+  final picture2;
+  final picture3;
+  final picture4;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: topPosition,
+      left: 0,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              width: pictureSizeWidth,
+              height: pictureSizeHeight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Stack(
+                  children: [
+                    Image.asset(picture1),
+                    Positioned(
+                      top: 1,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Button 1',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              width: pictureSizeWidth,
+              height: pictureSizeHeight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Stack(
+                  children: [
+                    Image.asset(picture2),
+                    Positioned(
+                      top: 1,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Button 1',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              width: pictureSizeWidth,
+              height: pictureSizeHeight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Stack(
+                  children: [
+                    Image.asset(picture3),
+                    Positioned(
+                      top: 1,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Button 1',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          InkWell(
+            onTap: () {},
+            child: Container(
+                width: pictureSizeWidth,
+                height: pictureSizeHeight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Stack(
+                  children: [
+                    Image.asset(picture4),
+                    Positioned(
+                      top: 1,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Button 1',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
