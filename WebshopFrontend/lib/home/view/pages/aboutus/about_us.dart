f4eb95c78aed334ed8143ce4b321cf8a7ec9eed3
Emilia Müller <emilia.mueller@edu.fhdw.de>
2023-04-29 23:15:17 +0200

    creation of About Us page - writing the heading

--- WebshopFrontend/lib/home/view/pages/aboutus/about_us.dart
+++ WebshopFrontend/lib/home/view/pages/aboutus/about_us.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfrontend/constats.dart';
+import 'package:flutterfrontend/home/view/pages/shop/first_shopscreen.dart';
 
-class AboutUs extends StatelessWidget{
+
+class AboutUs extends StatelessWidget {
+  const AboutUs({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      child: Text('HelloWorld!'),
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Stack(
+      // alignment: AlignmentDirectional.center,
+      children: [
+        Positioned(
+          top: screenHeight*0.1,
+          left: screenWidth*0.05,
+          right: 0,
+            child: Row(
+
+              children: [
+                Text('Über uns', textAlign: TextAlign.left, style: TextStyle(
+                  fontSize: 30, fontWeight: FontWeight.bold, color: schemeColorGreen,
+                ),)
+              ],
+            )
+        )
+      ],
     );
   }
-
 }
+
 
