ff0f17fa9895edeb86a07cfbf9551f6252ca7b14
Florian Brunner <florian-home@web.de>
2023-04-30 10:48:33 +0200

    Add text to Pictures

---
+++ WebshopFrontend/lib/globalwidget/roundet_text_picture.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../constats.dart';
+
+class RoundetTextPicture extends StatelessWidget {
+  const RoundetTextPicture({
+    super.key,
+    required this.pictureSizeWidth,
+    required this.pictureSizeHeight,
+    required this.picture,
+    required this.headLineText,
+  });
+
+  final pictureSizeWidth;
+  final pictureSizeHeight;
+  final picture;
+  final headLineText;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        width: pictureSizeWidth,
+        height: pictureSizeHeight,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Image.asset( picture,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                top: 10,
+                left: 0,
+                right: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        width: pictureSizeWidth -30,
+                        color: schemeColorGreen,
+                        child: Text(
+                          headLineText,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          )
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- WebshopFrontend/lib/home/view/pages/shop/first_shopscreen.dart
+++ WebshopFrontend/lib/home/view/pages/shop/first_shopscreen.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import '../../../../globalwidget/roundet_text_picture.dart';
 
 class FirstShopScreen extends StatelessWidget {
 
@@ -13,25 +14,36 @@ class FirstShopScreen extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
+    final topRowPosition = screenHeight * 0.08;
+    final bottomRowPosition = topRowPosition + 350;
+
     return Stack(
       children: [
         FourPictureRow(
-          topPosition: screenHeight * 0.08,
+          topPosition: topRowPosition,
           pictureSizeHeight: 300,
           pictureSizeWidth: 300,
           picture1: '../assets/StartScreen.png',
+          headLineText1: 'Shop 1',
           picture2: '../assets/StartScreen.png',
+          headLineText2: 'Shop 2',
           picture3: '../assets/StartScreen.png',
+          headLineText3: 'Shop 3',
           picture4: '../assets/StartScreen.png',
+          headLineText4: 'Shop 4',
         ),
         FourPictureRow(
-          topPosition: screenHeight * 0.5,
+          topPosition: bottomRowPosition,
           pictureSizeHeight: 300,
           pictureSizeWidth: 300,
           picture1: '../assets/StartScreen.png',
+          headLineText1: 'Shop 5',
           picture2: '../assets/StartScreen.png',
+          headLineText2: 'Shop 6',
           picture3: '../assets/StartScreen.png',
+          headLineText3: 'Shop 7',
           picture4: '../assets/StartScreen.png',
+          headLineText4: 'Shop 8',
         ),
       ],
     );
@@ -40,7 +52,18 @@ class FirstShopScreen extends StatelessWidget {
 
 class FourPictureRow extends StatelessWidget {
   const FourPictureRow({
-    super.key, required this.topPosition, required this.pictureSizeWidth, required this.pictureSizeHeight, required this.picture1, required this.picture2,  required this.picture3, required this.picture4,
+    super.key,
+    required this.topPosition,
+    required this.pictureSizeWidth,
+    required this.pictureSizeHeight,
+    required this.picture1,
+    required this.picture2,
+    required this.picture3,
+    required this.picture4,
+    required this.headLineText1,
+    required this.headLineText2,
+    required this.headLineText3,
+    required this.headLineText4
   });
 
   final topPosition;
@@ -50,6 +73,10 @@ class FourPictureRow extends StatelessWidget {
   final picture2;
   final picture3;
   final picture4;
+  final headLineText1;
+  final headLineText2;
+  final headLineText3;
+  final headLineText4;
 
   @override
   Widget build(BuildContext context) {
@@ -60,124 +87,18 @@ class FourPictureRow extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          InkWell(
-            onTap: () {},
-            child: Container(
-              width: pictureSizeWidth,
-              height: pictureSizeHeight,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Stack(
-                  children: [
-                    Image.asset(picture1),
-                    Positioned(
-                      top: 1,
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Button 1',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          RoundetTextPicture(pictureSizeWidth: pictureSizeWidth, pictureSizeHeight: pictureSizeHeight, picture: picture1, headLineText: headLineText1),
           SizedBox(width: 20),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              width: pictureSizeWidth,
-              height: pictureSizeHeight,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Stack(
-                  children: [
-                    Image.asset(picture2),
-                    Positioned(
-                      top: 1,
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Button 1',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          RoundetTextPicture(pictureSizeWidth: pictureSizeWidth, pictureSizeHeight: pictureSizeHeight, picture: picture2, headLineText: headLineText2),
           SizedBox(width: 20),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              width: pictureSizeWidth,
-              height: pictureSizeHeight,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Stack(
-                  children: [
-                    Image.asset(picture3),
-                    Positioned(
-                      top: 1,
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Button 1',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          RoundetTextPicture(pictureSizeWidth: pictureSizeWidth, pictureSizeHeight: pictureSizeHeight, picture: picture3, headLineText: headLineText3),
           SizedBox(width: 20),
-          InkWell(
-            onTap: () {},
-            child: Container(
-                width: pictureSizeWidth,
-                height: pictureSizeHeight,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Stack(
-                  children: [
-                    Image.asset(picture4),
-                    Positioned(
-                      top: 1,
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Button 1',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          RoundetTextPicture(pictureSizeWidth: pictureSizeWidth, pictureSizeHeight: pictureSizeHeight, picture: picture4, headLineText: headLineText4),
         ],
       ),
     );
   }
 }
+
+
 
