e6b147578dac87605d89a343cbca7c077809913b
Florian Brunner <florian-home@web.de>
2023-05-09 19:38:13 +0200

    working item data loading

--- WebshopFrontend/lib/home/view/pages/produkt/poduct_service.dart
+++ WebshopFrontend/lib/home/view/pages/produkt/poduct_service.dart
@@ -11,16 +11,15 @@ Future<ProductSummary> fetchProduct(String id) async {
 
   response = await http.get(Uri.parse(apiPathProductData + id));
   if (response.statusCode == 200) {
-    final dynamic data = jsonDecode(response.body);
-    return data.map((json) =>
-        ProductSummary(
+    final dynamic json = jsonDecode(response.body);
+    return ProductSummary(
           name: json['name'],
           description: json['description'],
           id: json['id'],
           category: json['category'],
           subCategory: json['subCategory'],
           price: Decimal.parse(json['price']),
-        )).toList();
+        );
   } else {
     throw Exception('Failed to fetch products${response.statusCode}');
   }
--- WebshopFrontend/lib/home/view/pages/produkt/product_page.dart
+++ WebshopFrontend/lib/home/view/pages/produkt/product_page.dart
@@ -18,90 +18,117 @@ class ProductPage extends StatefulWidget {
 
 class _ProductPageState extends State<ProductPage> {
   late Future <ProductSummary> productData;
+
+
+  @override
+  void initState() {
+    super.initState();
+    productData = fetchProduct(widget.productId);
+  }
+
   @override
   Widget build(BuildContext context) {
-    productData = fetchProduct(widget.productId);
-    print(productData);
-    return CenteredView(
-      child:
-      Scaffold(
-        appBar: TopBar(ueberUns: false,),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height : double.infinity),
-            // Image Selection
-            Container(
-              width: 90,
-              height: 600,
-              child: ListView.builder(
-                itemCount: 7, // Replace with the actual number of images
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 80,
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    color: Colors.grey[300], // Placeholder color
-                    child: Center(
-                      child: Text('Image ${index + 1}'),
+    return FutureBuilder<ProductSummary>(
+      future: productData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final productSummary = snapshot.data!;
+          return CenteredView(
+            child:
+            Scaffold(
+              appBar: TopBar(ueberUns: false,),
+              body: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: double.infinity),
+                  // Image Selection
+                  Container(
+                    width: 90,
+                    height: 600,
+                    child: ListView.builder(
+                      itemCount: 7,
+                      // Replace with the actual number of images
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 80,
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          color: Colors.grey[300], // Placeholder color
+                          child: Center(
+                            child: Text('Image ${index + 1}'),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(width: 20),
-            // Big Product Image
-            CachedNetworkImage(imageUrl: '$apiPathPicture${widget.productId}/image1',
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-              height: 600,
-              width: 450,
-              fit: BoxFit.fitHeight,),
-            SizedBox(width: 80),
-            // Product Details
-            Container(
-              height: 600,
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Product Name',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                  ),
+                  SizedBox(width: 20),
+                  // Big Product Image
+                  CachedNetworkImage(
+                    imageUrl: '$apiPathPicture${widget.productId}/image1',
+                    placeholder: (context, url) =>
+                        CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                    height: 600,
+                    width: 450,
+                    fit: BoxFit.fitHeight,),
+                  SizedBox(width: 80),
+                  // Product Details
+                  Container(
+                    height: 600,
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            productSummary.name,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '${productSummary.price}€',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: schemeColorGreen,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '${productSummary.description}',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              // TODO: Implement add to basket functionality
+                            },
+                            child: Text('Dem Warenkorb hinzufügen'),
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              // TODO: Implement quick checkout functionality
+                            },
+                            child: Text('Direkt zur Kasse'),
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(height: 10),
-                    Text(
-                      'productData.price as String',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: schemeColorGreen,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        // TODO: Implement add to basket functionality
-                      },
-                      child: Text('Dem Warenkorb hinzufügen'),
-                    ),
-                    SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        // TODO: Implement quick checkout functionality
-                      },
-                      child: Text('Direkt zur Kasse'),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return CircularProgressIndicator();
+        }
+      });
   }
 }
