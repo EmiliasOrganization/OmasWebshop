bcddc331e6c9df4f3d92fde4eb5cecaa2fc77fb6
Florian Brunner <florian-home@web.de>
2023-05-12 14:32:52 +0200

    enable Docker for Rocket

--- UserAndAuthBackend/src/main.rs
+++ UserAndAuthBackend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+//noinspection RsMainFunctionNotFound
+
+#[macro_use] extern crate rocket;
+use rocket::{Build, Rocket, Request};
+
+#[catch(500)]
+fn internal_error() -> &'static str {
+    "Whoops! Looks like we messed up."
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("I couldn't find '{}'. Try something else?", req.uri())
+}
+
+#[get("/world")]
+fn hi() -> &'static str {
+    "Hello!"
+}
+
+#[get("/hello")]
+fn ho() -> &'static str {
+    "world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![hi, ho])
+        .register("/", catchers![internal_error, not_found])
 }
