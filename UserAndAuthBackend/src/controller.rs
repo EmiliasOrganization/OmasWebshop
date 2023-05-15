fbb458f252c8d6ea6d7ff0d428759ea75606db5d
Florian Brunner <florian-home@web.de>
2023-05-15 12:46:38 +0200

    add documentation with swagger

--- UserAndAuthBackend/src/controller.rs
+++ UserAndAuthBackend/src/controller.rs
@@ -4,7 +4,6 @@ use rocket::serde::json::{Json};
 use crate::models::{Login, User};
 use crate::service::{create_user_service, login_user_service};
 
-
 // catchers
 
 #[catch(500)]
@@ -27,12 +26,27 @@ pub fn missing_entity() -> &'static str { "Invalid input" }
 
 // routes
 
+#[utoipa::path(
+    context_path = "/api/user",
+    request_body = User,
+    responses(
+        (status = 200, body = User)
+    )
+)]
 #[post("/register", data = "<body>", format = "json")]
 pub async fn register(body: Json<User>) -> Result<String, Status>
 {
      create_user_service(body).await
 }
 
+
+#[utoipa::path(
+    context_path = "/api/auth",
+    request_body = Login,
+    responses(
+        (status = 200, description = "Login successful", body = Address)
+    )
+)]
 #[post("/login", data = "<body>", format = "json")]
 pub async fn login(body: Json<Login>) -> Result<String, Status>
 {
--- UserAndAuthBackend/src/main.rs
+++ UserAndAuthBackend/src/main.rs
@@ -22,12 +22,35 @@ use controller::{
 
 #[macro_use] extern crate rocket;
 use rocket::{Build, catchers, Rocket, routes};
+use utoipa_swagger_ui::SwaggerUi;
+use utoipa::OpenApi;
+use crate::controller::__path_login;
+use crate::controller::__path_register;
+use crate::models::Login;
+use crate::models::User;
+use crate::models::Address;
 
 #[launch]
 fn rocket() -> Rocket<Build> {
+
+
+
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(
+            login,
+            register,
+        ),
+        components(
+            schemas(Login, User, Address),
+        )
+    )]
+    struct ApiDoc;
+
     rocket::build()
         .mount("/api/auth", routes![register, login])
         .register("/", catchers![internal_error, not_found, conflict, missing_entity])
         .register("/api/auth/login", catchers![user_not_found])
+        .mount("/", SwaggerUi::new("/swagger-ui/<_..>").url("/api-docs/openapi.json", ApiDoc::openapi()))
 }
 
