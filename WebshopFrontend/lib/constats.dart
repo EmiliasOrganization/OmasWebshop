8e09bfcd3527c8ae7785e903f4368f9096a7c4ed
Florian Brunner <florian-home@web.de>
2023-06-26 20:27:03 +0200

    further refactoring

--- WebshopFrontend/lib/constats.dart
+++ WebshopFrontend/lib/constats.dart
@@ -17,21 +17,13 @@ const Color schemeColorGreen = Color(0xff4A6840);
 const Color schemeColorMistyRose= Color(0xFFF6DEE0);
 
 // Api Paths
-/**Shop Api */
-const String apiPathProductSummary = 'http://localhost:8080/api/shop/all';
-const String apiPathCategoryAndOrSubCategory = 'http://localhost:8080/api/shop/category';
-const String apiPathOnlySubCategory = 'http://localhost:8080/api/shop/subcategory';
-const String apiPathPicture = 'http://localhost:8080/api/shop/picture/';
-const String apiPathProductData = 'http://localhost:8080/api/shop/productSummary/';
-const String apiPathImageCount = 'http://localhost:8080/api/shop/picture/count/';
-/** Auth Api*/
-const String authApiRegister = 'http://localhost:8000/api/auth/register';
+
 ///Shop Api
 const String shopApi = 'http://localhost:8080/api/shop';
 ///Auth Api
 const String authApiVerify = 'http://localhost:8000/api/auth/register/verify';
 const String authApiLogin = 'http://localhost:8000/api/auth/login';
-
+const String authApiRegister = 'http://localhost:8000/api/auth/register';
 // Build something Backend and Frontend can read to generify it
 //Shop Categories
 enum Category {
